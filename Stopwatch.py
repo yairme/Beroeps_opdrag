07975981e43860c8c7b14baa08e3e26ed532c705
yairme <yairstward2013@gmail.com>
2020-10-23 14:29:32 +0200

    Add files via upload

---
+++ Stopwatch.py
@@ -0,0 +1,11 @@
+from microbit import *
+
+def on_button_pressed_a():
+    global start
+    start = input.running_time()
+    input.on_button_pressed(Button.A, on_button_pressed_a)
+
+    def on_button_pressed_b():
+        elapsed = input.running_time() - start
+        basic.show_number(Math.idiv(elapsed, 1000))
+    input.on_button_pressed(Button.B, on_button_pressed_b)
